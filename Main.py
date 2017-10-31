8c3dda9ad8d6c4f8977cd65b89e74aae5582066a
moraw <krzymoraw@gmail.com>
2017-10-31 14:06:53 +0100

    Corrected equations for envelope functions. Added function for saving series of 2D PCTF images

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import ImageSupport as imsup
 
 # ---------------------------------------------------------------
 
-def calc_ctf_1d(img_dim, px_dim, ewf_lambda, defocus, Cs=0.0, df_spread=0.0, conv_angle=0.0):
+def calc_ctf_1d(img_dim, px_dim, ewf_lambda, defocus, Cs=0.0, df_spread=0.0, conv_angle=0.0, fname='pctf1d'):
     df_coeff = np.pi * ewf_lambda * defocus
     Cs_coeff = 0.5 * np.pi * (ewf_lambda ** 3) * Cs
 
@@ -17,37 +17,39 @@ def calc_ctf_1d(img_dim, px_dim, ewf_lambda, defocus, Cs=0.0, df_spread=0.0, con
     k_squared = kx ** 2
 
     aberr_fun = df_coeff * k_squared + Cs_coeff * (k_squared ** 2)
-    pctf = np.sin(aberr_fun)
+    pctf = -np.sin(aberr_fun)
 
-    spat_env_fun = np.exp(-(np.pi * conv_angle * defocus * kx) ** 2)
-    temp_env_fun = np.exp(-(np.pi * ewf_lambda * df_spread * (kx ** 2)) ** 2)
+    spat_env_fun = np.exp(-((np.pi * conv_angle * kx) ** 2) * (defocus + Cs * ewf_lambda ** 2 * k_squared) ** 2)
+    temp_env_fun = np.exp(-(0.5 * np.pi * ewf_lambda * df_spread * k_squared) ** 2)
 
     pctf *= spat_env_fun
     pctf *= temp_env_fun
 
     kx *= 1e-9
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 8))
     ax.plot(kx, pctf, 'r', label='PCTF')
-    ax.plot(kx, spat_env_fun, 'g', label='Spatial envelope')
-    ax.plot(kx, temp_env_fun, 'b', label='Temporal envelope')
-    # legend = ax.legend(loc='lower right', shadow=True)
+    ax.plot(kx, spat_env_fun, 'g', label='Spatial coherence envelope')
+    ax.plot(kx, temp_env_fun, 'b', label='Temporal coherence envelope')
+    legend = ax.legend(loc='lower right')
 
     plt.xlim([0, kx[-1]])
-    plt.ylim(bottom=-1)
+    plt.ylim([-1.1, 1.1])
     plt.axhline(0, color='k', lw=1.0)
-    plt.xlabel('k [nm-1]')
-    plt.legend()
-    # plt.show()
-    fig.savefig('foo.png', dpi=200)
+    plt.xlabel('Spatial frequency k [nm-1]')
+    plt.ylabel('Contrast')
+    plt.annotate('df = {0:.0f} nm'.format(defocus * 1e9), xy=(0, 0), xytext=(9.5, 0.9), fontsize=16)
+    # plt.legend()
+    fig.savefig('{0}.png'.format(fname))
+    plt.close(fig)
 
-    # pctf_to_save = np.vstack((kx * 1e-9, pctf, spat_env_fun, temp_env_fun)).transpose()
+    # pctf_to_save = np.vstack((kx, pctf, spat_env_fun, temp_env_fun)).transpose()
     # np.savetxt('pctf1d.txt', pctf_to_save, delimiter='\t')
     print('Done')
     return pctf
 
 # ---------------------------------------------------------------
 
-def calc_ctf_2d(img_dim, px_dim, ewf_lambda, defocus, Cs=0.0):
+def calc_ctf_2d(img_dim, px_dim, ewf_lambda, defocus, Cs=0.0, df_spread=0.0, conv_angle=0.0, fname='pctf2d'):
     df_coeff = np.pi * ewf_lambda * defocus
     Cs_coeff = 0.5 * np.pi * (ewf_lambda ** 3) * Cs
 
@@ -60,55 +62,52 @@ def calc_ctf_2d(img_dim, px_dim, ewf_lambda, defocus, Cs=0.0):
     k_squared = kx ** 2 + ky ** 2
 
     aberr_fun = df_coeff * k_squared + Cs_coeff * (k_squared ** 2)
-
     pctf = np.sin(aberr_fun)
+
+    spat_env_fun = np.exp(-((np.pi * conv_angle * kx) ** 2) * (defocus + Cs * ewf_lambda ** 2 * k_squared) ** 2)
+    temp_env_fun = np.exp(-(0.5 * np.pi * ewf_lambda * df_spread * k_squared) ** 2)
+
+    pctf *= spat_env_fun
+    pctf *= temp_env_fun
 
     pctf_img = imsup.ImageWithBuffer(img_dim, img_dim)
     pctf_img.LoadAmpData(pctf)
-    imsup.SaveAmpImage(pctf_img, 'pctf2d.png')
+    imsup.SaveAmpImage(pctf_img, '{0}.png'.format(fname))
 
     print('Done')
     return pctf
 
 # ---------------------------------------------------------------
 
-def save_range_of_ctf_images(img_dim, px_dim, ewf_lambda, df_pars, Cs=0.0, df_spread=0.0, conv_angle=0.0):
+def save_range_of_ctf_1d_images(img_dim, px_dim, ewf_lambda, df_pars, Cs=0.0, df_spread=0.0, conv_angle=0.0, fname='pctf1d'):
+
     df_min, df_max, df_step = df_pars
-    Cs_coeff = 0.5 * np.pi * (ewf_lambda ** 3) * Cs
-    rec_px_dim = 1.0 / (img_dim * px_dim)
-    x = np.arange(0, img_dim // 2, 1)
-    kx = x * rec_px_dim
-    k_squared = kx ** 2
-    temp_env_fun = np.exp(-0.5 * ((np.pi * ewf_lambda * df_spread) ** 2) * (kx ** 4))       # 0.5 czy nie?
-    f_idx = 0
+    df_values = np.arange(df_min, df_max, df_step)
+    for df, idx in zip(df_values, range(df_values.shape[0])):
+        fn = '{0}_{1}'.format(fname, idx + 1)
+        calc_ctf_1d(img_dim, px_dim, ewf_lambda, df, Cs, df_spread, conv_angle, fn)
 
-    for df in np.arange(df_min, df_max, df_step):
-        f_idx += 1
-        df_coeff = np.pi * ewf_lambda * df
-        aberr_fun = df_coeff * k_squared + Cs_coeff * (k_squared ** 2)
-        pctf = np.sin(aberr_fun)
-        spat_env_fun = np.exp(-(np.pi * conv_angle * df * kx) ** 2)
-        pctf *= spat_env_fun
-        pctf *= temp_env_fun
+    print('All done')
 
-        fig, ax = plt.subplots(figsize=(10, 8))
-        ax.plot(kx * 1e-9, pctf, 'r', label='PCTF')
-        ax.plot(kx * 1e-9, spat_env_fun, 'g', label='Spatial envelope')
-        ax.plot(kx * 1e-9, temp_env_fun, 'b', label='Temporal envelope')
-        legend = ax.legend(loc='lower right')
-
-        plt.xlim([0, kx[-1] * 1e-9])
-        plt.ylim([-1, 1.1])
-        plt.axhline(0, color='k', lw=1.0)
-        plt.xlabel('Spatial frequency k [nm-1]')
-        plt.ylabel('Contrast')
-        plt.annotate('df = {0:.0f} nm'.format(df * 1e9), xy=(0, 0), xytext=(11, 1))
-        # plt.legend()
-        fig.savefig('foo{0}.png'.format(f_idx))
-
-    print('Done')
     return
 
 # ---------------------------------------------------------------
 
-save_range_of_ctf_images(1024, 40e-12, const.ewfLambda, [0e-9, 100e-9, 10e-9], Cs=0.6e-3, df_spread=3e-9, conv_angle=1e-3)
+def save_range_of_ctf_2d_images(img_dim, px_dim, ewf_lambda, df_pars, Cs=0.0, df_spread=0.0, conv_angle=0.0, fname='pctf2d'):
+
+    df_min, df_max, df_step = df_pars
+    df_values = np.arange(df_min, df_max, df_step)
+    for df, idx in zip(df_values, range(df_values.shape[0])):
+        fn = '{0}_{1}'.format(fname, idx+1)
+        calc_ctf_2d(img_dim, px_dim, ewf_lambda, df, Cs, df_spread, conv_angle, fn)
+
+    print('All done')
+    return
+
+# ---------------------------------------------------------------
+
+# calc_ctf_1d(1024, 40e-12, const.ewfLambda, defocus=0e-9, Cs=0.6e-3, df_spread=4e-9, conv_angle=0.25e-3)
+save_range_of_ctf_1d_images(1024, 40e-12, const.ewfLambda, [0e-9, 1050e-9, 50e-9], Cs=0.6e-3, df_spread=4e-9, conv_angle=0.25e-3)
+
+# calc_ctf_2d(1024, 40e-12, const.ewfLambda, defocus=0e-9, Cs=0.6e-3, df_spread=4e-9, conv_angle=0.25e-3)
+# save_range_of_ctf_2d_images(1024, 40e-12, const.ewfLambda, [0e-9, 1050e-9, 50e-9], Cs=0.6e-3, df_spread=4e-9, conv_angle=0.25e-3)
